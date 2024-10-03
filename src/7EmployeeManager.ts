831adfbeaa3255c045dbdc886d92a504b32200c1
vdumchiviy <brain1999@mail.ru>
2024-10-03 16:49:45 -0400

    step1

--- src/7EmployeeManager.ts
+++ src/7EmployeeManager.ts
@@ -1,8 +1,39 @@
 import {randomBytes} from 'crypto'
+import { create } from 'domain'
 
 type Employee = {
     name: string
     id: string
     email: string
-    salary: string
+    salary: number
 }
+
+function generateRandomId() {
+    return randomBytes(10).toString('hex');
+}
+function createEmployee(
+    employeeName: string,
+    salary: number
+) : Employee {
+
+    return {
+        name: employeeName,
+        id: generateRandomId(),
+        email: `${employeeName}@example.com`,
+        salary: salary
+    }
+}
+
+const Peter = createEmployee('Peter', 10000);
+const Dan = createEmployee('Dan', 80000);
+const allNewEmployees = [Peter, Dan]
+
+const sendWelcomeMessage = (employee: Employee) => {
+    console.log(`To: ${employee.email}
+        Hello ${employee.name}!
+        Welcome to the company!
+        Your salary will be ${employee.salary}
+        `);
+}
+
+allNewEmployees.forEach(emp => sendWelcomeMessage(emp))
