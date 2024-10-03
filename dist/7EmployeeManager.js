cc832e3580e1ee5913a48d1ebef45dc88099b3f5
vdumchiviy <brain1999@mail.ru>
2024-10-03 16:53:36 -0400

    tsc step1

---
+++ dist/7EmployeeManager.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto_1 = require("crypto");
+function generateRandomId() {
+    return (0, crypto_1.randomBytes)(10).toString('hex');
+}
+function createEmployee(employeeName, salary) {
+    return {
+        name: employeeName,
+        id: generateRandomId(),
+        email: `${employeeName}@example.com`,
+        salary: salary
+    };
+}
+const Peter = createEmployee('Peter', 10000);
+const Dan = createEmployee('Dan', 80000);
+const allNewEmployees = [Peter, Dan];
+const sendWelcomeMessage = (employee) => {
+    console.log(`To: ${employee.email}
+        Hello ${employee.name}!
+        Welcome to the company!
+        Your salary will be ${employee.salary}
+        `);
+};
+allNewEmployees.forEach(emp => sendWelcomeMessage(emp));
